1f2cb916db84ed1f65fd57975658af20f1f80ebe
mahdi791211 <mohammadzademahdi70@gmail.com>
2024-11-26 14:22:59 +0330

    first commit

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const CharactersCollection_1 = require("./CharactersCollection");
+const charactersCollection = new CharactersCollection_1.CharactersCollection('iufhbguihYGFHFGklfojv');
+charactersCollection.sort();
+console.log(charactersCollection.data);
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
+
+
+const charactersCollection = new CharactersCollection('iufhbguihYGFHFGklfojv');
+charactersCollection.sort();
+console.log(charactersCollection.data)
